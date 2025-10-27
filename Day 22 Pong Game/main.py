136dfd9468d0cef204d4c02c34b589388c581257
jhonny46 <71387965+jhonny46@users.noreply.github.com>
2025-10-26 21:47:56 -0700

    updated the snake main added scorboard. Counts score for each side.

--- Day 22 Pong Game/main.py
+++ Day 22 Pong Game/main.py
@@ -1,7 +1,9 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
+import time
 
 screen = Screen()
 
@@ -10,6 +12,7 @@ screen.setup(800, 600)
 screen.title("Pong")
 
 screen.tracer(0)
+scoarboard = Scoreboard()
 
 ball = Ball()
 l_paddel = Paddle((350,0))
@@ -26,7 +29,24 @@ screen.onkey(r_paddel.go_down,"s")
 is_game_on = True
 
 while is_game_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+    # Detect collion with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    # Detect collision with padel
+    if ball.distance(l_paddel) < 50 and ball.xcor() > 320 or ball.distance(r_paddel) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddel
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoarboard.l_point()
+    # Detect l paddel
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoarboard.r_point()
 
 screen.exitonclick()
 
